73921e0b90ea9f47fa5475a7a034dcc01442387f
Anna Bush <annabush092@gmail.com>
2020-09-14 16:49:04 -0400

    first commit

---
+++ TestCircularGauge/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestCircularGauge
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzAzNDQyQDMxMzgyZTMyMmUzMGZVdTdGZGo3VUpSNGw2cDNtdjVnMlJyOGZDSWY4RDE5RTVHOHNMeFNYbVU9");
+
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
